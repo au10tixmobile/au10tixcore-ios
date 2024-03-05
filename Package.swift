a9035565d11bd9cb6d7ead24cd151bbb05b7b34a
au10tixmobile <94300864+au10tixmobile@users.noreply.github.com>
2024-03-05 16:40:38 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+/*
+ * Copyright (c) Au10tix.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import PackageDescription
+import Foundation
+
+let package = Package(
+    name: "au10tixcore-ios",
+    products: [ 
+        .library(name: "Au10tixCore", targets: ["Au10tixCore"])
+    ],
+    targets: [
+        .binaryTarget(
+          name: "Au10tixCore",
+          url: "https://github.com/au10tixmobile/Au10tixCore/archive/refs/tags/4.0.0.zip",
+          checksum: "ed5691a5d3bf0e519daa6c0e814c9900d77bd6db00aac68b1931afc87d6683f6"
+        )
+    ]
+)
