e9a0999209209d1ae4c45c3985ffad76b04ad9d9
au10tixmobile <94300864+au10tixmobile@users.noreply.github.com>
2025-04-17 17:18:21 +0300

    Update Package.swift
    Used release asset in binary target download

--- Package.swift
+++ Package.swift
@@ -19,8 +19,10 @@ let package = Package(
     targets: [
         .binaryTarget(
           name: "Au10tixCore",
-          url: "https://github.com/au10tixmobile/Au10tixCore/archive/refs/tags/4.3.0.zip",
-          checksum: "e47a1a42a688c2aa9ad75ce4dcc7c6fb97ff4afaabfbbc0fea9655e7ce905e8e"
+          // url: "https://github.com/au10tixmobile/Au10tixCore/archive/refs/tags/4.3.0.zip",
+          // checksum: "e47a1a42a688c2aa9ad75ce4dcc7c6fb97ff4afaabfbbc0fea9655e7ce905e8e"
+          url: "https://github.com/au10tixmobile/Au10tixCore/releases/download/4.3.0/Au10tixCore.xcframework.zip",
+          checksum: "b93f55a24b637c997ebdd1d298eea16ff226ec83e69a003dd91d2860074f1f25"
         )
     ]
 )
